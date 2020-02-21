28e5cd8f4c2fb9a0eaf88b360ad1073aac546dc5
Qi Li <liqi17thu@gmail.com>
2020-02-21 09:28:56 +0800

    Tensorboard

--- config.py
+++ config.py
@@ -39,6 +39,7 @@ def get_args():
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight-decay', type=float, default=1e-4)
     parser.add_argument('--print-interval', type=int, default=100)
+    parser.add_argument('--display', type=int, default=20)
     parser.add_argument('--cuda', type=bool, default=True)
     parser.add_argument('--pretrained-model', type=bool, default=False)
     parser.add_argument('--stem', type=bool, default=False, help='attention stem: True, conv: False')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 
 import os
 import shutil
@@ -19,7 +20,7 @@ def adjust_learning_rate(optimizer, epoch, args):
         param_group['lr'] = lr
 
 
-def train(model, train_loader, optimizer, criterion, epoch, args, logger):
+def train(model, train_loader, optimizer, criterion, epoch, args, logger, writer):
     top1 = AvgrageMeter()
     top5 = AvgrageMeter()
     losses = AvgrageMeter()
@@ -27,7 +28,8 @@ def train(model, train_loader, optimizer, criterion, epoch, args, logger):
     model.train()
 
     step = 0
-    for data, target in train_loader:
+    sta_time = time.time()
+    for i, (data, target) in enumerate(train_loader):
         adjust_learning_rate(optimizer, epoch, args)
         if args.cuda:
             data, target = data.cuda(), target.cuda()
@@ -48,33 +50,51 @@ def train(model, train_loader, optimizer, criterion, epoch, args, logger):
 
         step += 1
         if step % args.print_interval == 0:
-            logger.info("[Epoch {0:4d}] Loss {losses.avg:.3f} Prec@(1,5)" 
-                        "({top1.avg:.1%}, {top5.avg:.1%})".format(epoch, losses=losses, top1=top1, top5=top5))
-            for param_group in optimizer.param_groups:
-                logger.info("Current learning rate is: {}".format(param_group['lr']))
+            logger.info("Train: Epoch {}/{}  Time: {:.3f} Loss {losses.avg:.3f} LR {:.3f} "
+                        "Prec@(1,5) ({top1.avg:.1%}, {top5.avg:.1%})".format(
+                        epoch, args.epochs, (time.time()-sta_time)/args.display,
+                        optimizer.param_groups[0]['lr'],
+                        losses=losses, top1=top1, top5=top5))
+
+            writer.add_scalar('Loss/train', losses.avg, epoch * len(train_loader) + i)
+            writer.add_scalar('Accuracy/train', top1.avg, epoch * len(train_loader) + i)
 
 
-def eval(model, test_loader, args):
+def eval(model, test_loader, criterion, epoch, args, writer):
     print('evaluation ...')
     model.eval()
 
     top1 = AvgrageMeter()
     top5 = AvgrageMeter()
+    losses = AvgrageMeter()
 
+
+    step = 0
+    sta_time = time.time()
     with torch.no_grad():
-        for data, target in test_loader:
+        for i, (data, target) in enumerate(test_loader):
             N = data.size(0)
-
             if args.cuda:
                 data, target = data.cuda(), target.cuda()
             output = model(data)
 
-            top1.update(prec1.item(), N)
-            top5.update(prec5.item(), N)
+            loss = criterion(output, target)
             prec1, prec5 = accuracy(output, target, topk=(1, 5))
 
-    logger.info("Prec @ (1, 5)"
-    "({top1.avg:.1%}, {top5.avg:.1%})".format(top1=top1, top5=top5))
+            losses.update(loss.item(), N)
+            top1.update(prec1.item(), N)
+            top5.update(prec5.item(), N)
+
+            step += 1
+            if step % args.print_interval == 0:
+                logger.info("Train: Epoch {}/{}  Time: {:.3f} Loss {losses.avg:.3f} "
+                            "Prec@(1,5) ({top1.avg:.1%}, {top5.avg:.1%})".format(
+                    epoch, args.epochs, (time.time() - sta_time) / args.display,
+                    losses=losses, top1=top1, top5=top5))
+
+                writer.add_scalar('Loss/vaild', losses.avg, epoch * len(test_loader) + i)
+                writer.add_scalar('Accuracy/vaild', top1.avg, epoch * len(test_loader) + i)
+
     return top1
 
 
@@ -89,6 +109,8 @@ def get_model_parameters(model):
 
 
 def main(args, logger):
+    writer = SummaryWriter()
+
     train_loader, test_loader = load_data(args)
     if args.dataset == 'CIFAR10':
         num_classes = 10
@@ -136,8 +158,8 @@ def main(args, logger):
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     for epoch in range(start_epoch, args.epochs + 1):
-        train(model, train_loader, optimizer, criterion, epoch, args, logger)
-        eval_acc = eval(model, test_loader, args)
+        train(model, train_loader, optimizer, criterion, epoch, args, logger, writer)
+        eval_acc = eval(model, test_loader, criterion, epoch, args, writer)
 
         is_best = eval_acc > best_acc
         best_acc = max(eval_acc, best_acc)

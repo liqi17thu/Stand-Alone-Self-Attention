3d3b989dcd002580e3cb64574b65d69367579cbd
Qi Li <liqi17thu@gmail.com>
2020-02-22 15:02:52 +0800

    standard self-attention

--- lib/models/units/saUnit.py
+++ lib/models/units/saUnit.py
@@ -3,25 +3,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
 
+import numpy as np
+
 from lib.models.units.utils import get_same_padding
 
+
 class SAConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, heads=1, bias=False):
         super(SAConv, self).__init__()
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        self.groups = groups
+        self.heads = heads
 
-        assert self.out_channels % self.groups == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
+        # here we actually assume that hv = hk = out_channels / heads
+        assert self.out_channels % self.heads == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
 
         self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
         self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
 
-        self.key_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, groups=groups)
-        self.query_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, groups=groups)
-        self.value_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, groups=groups)
+        self.key_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
+        self.query_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
+        self.value_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
+        self.project_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=bias)
 
         self.reset_parameters()
 
@@ -39,15 +44,15 @@ class SAConv(nn.Module):
         k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
         k_out = torch.cat((k_out_h + self.rel_h, k_out_w + self.rel_w), dim=1)
 
-        k_out = k_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
-        v_out = v_out.contiguous().view(batch, self.groups, self.out_channels // self.groups, height, width, -1)
+        k_out = k_out.contiguous().view(batch, self.heads, self.out_channels // self.heads, height, width, -1)
+        v_out = v_out.contiguous().view(batch, self.heads, self.out_channels // self.heads, height, width, -1)
+        q_out = q_out.view(batch, self.heads, self.out_channels // self.heads, height, width, 1)
 
-        q_out = q_out.view(batch, self.groups, self.out_channels // self.groups, height, width, 1)
-
-        out = (q_out * k_out).sum(dim=2, keepdim=True) * torch.sqrt((self.groups // self.out_channels).float())
+        out = (q_out * k_out).sum(dim=2, keepdim=True) * np.sqrt((self.heads // self.out_channels))
         out = F.softmax(out, dim=-1)
         out = (out * v_out).sum(dim=-1)
         out = out.view(batch, -1, height, width)
+        out = self.project_conv(out)
 
         return out
 
@@ -55,6 +60,7 @@ class SAConv(nn.Module):
         init.kaiming_normal_(self.key_conv.weight, mode='fan_out', nonlinearity='relu')
         init.kaiming_normal_(self.value_conv.weight, mode='fan_out', nonlinearity='relu')
         init.kaiming_normal_(self.query_conv.weight, mode='fan_out', nonlinearity='relu')
+        init.kaiming_normal_(self.project_conv.weight, mode='fan_out', nonlinearity='relu')
 
         init.normal_(self.rel_h, 0, 1)
         init.normal_(self.rel_w, 0, 1)

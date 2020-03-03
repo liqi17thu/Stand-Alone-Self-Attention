fce66d19b16321dc20df6edc400b9f77177b75a0
Qi Li <liqi17thu@gmail.com>
2020-03-03 18:27:15 +0800

    differen kind of encoding

---
+++ lib/models/units/postionalEncoding.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+
+import math
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, out_channels, kernel_size, heads, bias=False, encoding='learnable', r_dim=256):
+        super(PositionalEncoding, self).__init__()
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.heads = heads
+        self.encoding = encoding
+
+        if type == "learnable":
+            self.encoding = LearnablePostionalEncoding(out_channels, kernel_size)
+        elif type == "sine":
+            self.encoding = SinePositionalEncoding(out_channels // heads)
+        elif type == "xl":
+            self.encoding = XLPositionalEncoding(out_channels, heads, r_dim, bias)
+        else:
+            raise NotImplementedError
+
+    def forward(self, k_out, r=None):
+        # shape of K_out:
+        # batch, channels, height, width, kernel, kernel
+        if self.encoding == "learnable":
+            return self.encoding(k_out)
+        elif self.encoding == "sine":
+            batch, channels, height, width, kernel, kernel = k_out.size()
+            # batch, heads, out/heads, H/s, W/s, -1
+            k_out = k_out.contiguous().view(batch, self.heads, channels // self.heads, height, width, -1)
+            k_out = k_out.permute(0, 1, 3, 4, 2, 5).contiguous()
+            k_out = k_out.view(-1, channels // self.heads, kernel * kernel)
+            k_out = self.encoding(k_out)
+            k_out = k_out.view(batch, self.heads, height, width, channels // self.heads, -1)
+            k_out = k_out.permute(0, 1, 4, 2, 3, 5).contiguous()
+            return k_out.view(batch, channels, height, width, kernel, kernel)
+        elif self.encoding == "xl":
+            r_out, u, v = self.encoding(r)
+            return k_out, r_out, u, v
+        else:
+            raise NotImplementedError
+
+
+class LearnablePostionalEncoding(nn.Module):
+    def __int__(self, out_channels, kernel_size):
+        super(LearnablePostionalEncoding, self).__init__()
+        self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
+
+    def forward(self, k_out):
+        k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
+        return torch.cat((k_out_h + self.rel_h, k_out_w + self.rel_w), dim=1)
+
+
+class SinePositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=256):
+        super(SinePositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(d_model, max_len)
+        position = torch.arange(0, max_len, dtype=torch.float)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)).unsqueeze(1)
+        pe[0::2, :] = torch.sin(div_term * position)
+        pe[1::2, :] = torch.cos(div_term * position)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[0, :, :x.shape[2]]
+        return self.dropout(x)
+
+
+class XLPositionalEncoding(nn.Module):
+    def __init__(self, out_channels, heads, r_dim, bias=False):
+        super(XLPositionalEncoding, self).__init__()
+
+        self.u = nn.Parameter(torch.randn(1, heads, out_channels // heads, 1, 1, 1), requires_grad=True)
+        self.v = nn.Parameter(torch.randn(1, heads, out_channels // heads, 1, 1, 1), requires_grad=True)
+        self.key_position_conv = nn.Conv2d(r_dim, out_channels, kernel_size=1, bias=bias, groups=heads)
+
+    def forward(self, r):
+        r_out = self.key_position_conv(r)
+        r_out = r_out.view(1, self.groups, self.out_channels // self.groups, 1, 1, -1)
+        return r_out, self.u, self.v
--- lib/models/units/saUnit.py
+++ lib/models/units/saUnit.py
@@ -5,23 +5,25 @@ import torch.nn.init as init
 
 import numpy as np
 
-from .utils import get_same_padding, PositionalEncoding
+
+from .utils import get_same_padding
 from .activation import Hswish
+from .postionalEncoding import PositionalEncoding, SinePositionalEncoding
 
 
 class SAConv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, heads=1, bias=False):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, heads=1, bias=False, r_dim=256, encoding='learnable'):
         super(SAConv, self).__init__()
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
         self.heads = heads
+        self.encoding = encoding
 
         assert self.out_channels % self.heads == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
 
-        self.rel_h = nn.Parameter(torch.randn(out_channels // 2, 1, 1, kernel_size, 1), requires_grad=True)
-        self.rel_w = nn.Parameter(torch.randn(out_channels // 2, 1, 1, 1, kernel_size), requires_grad=True)
+        self.encoding = PositionalEncoding(out_channels, kernel_size, heads, bias, encoding, r_dim)
 
         self.key_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, groups=heads)
         self.query_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, stride=stride, groups=heads)
@@ -29,7 +31,7 @@ class SAConv(nn.Module):
 
         self.reset_parameters()
 
-    def forward(self, x):
+    def forward(self, x, r):
         batch, channels, height, width = x.size()
 
         padded_x = F.pad(x, [self.padding, self.padding, self.padding, self.padding])
@@ -40,15 +42,21 @@ class SAConv(nn.Module):
         k_out = k_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
         v_out = v_out.unfold(2, self.kernel_size, self.stride).unfold(3, self.kernel_size, self.stride)
 
-        k_out_h, k_out_w = k_out.split(self.out_channels // 2, dim=1)
-        k_out = torch.cat((k_out_h + self.rel_h, k_out_w + self.rel_w), dim=1)
+        # positonal encoding
+        if self.encoding == 'xl':
+            k_out, r_out, u, v = self.encoding(k_out, r)
+        else:
+            k_out = self.encoding(k_out)
 
         k_out = k_out.contiguous().view(batch, self.heads, self.out_channels // self.heads, height // self.stride, width // self.stride, -1)
         v_out = v_out.contiguous().view(batch, self.heads, self.out_channels // self.heads, height // self.stride, width // self.stride, -1)
-
         q_out = q_out.view(batch, self.heads, self.out_channels // self.heads, height // self.stride, width // self.stride, 1)
 
-        out = (q_out * k_out).sum(dim=2, keepdim=True) * np.sqrt(self.heads / self.out_channels)
+        if self.encoding == 'xl':
+            out = q_out * k_out + q_out * r_out + u * k_out + v * r_out
+        else:
+            out = q_out * k_out
+        out = out.sum(dim=2, keepdim=True) * np.sqrt((self.groups // self.out_channels))
         out = F.softmax(out, dim=-1)
         out = (out * v_out).sum(dim=-1)
         out = out.view(batch, -1, height // self.stride, width // self.stride)
@@ -75,7 +83,7 @@ class SAFull(nn.Module):
 
         assert self.out_channels % self.heads == 0, "out_channels should be divided by groups. (example: out_channels: 40, groups: 4)"
 
-        self.encoding = PositionalEncoding(out_channels)
+        self.encoding = SinePositionalEncoding(out_channels)
 
         self.key_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias, groups=heads)
         self.query_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=bias, groups=heads)
@@ -120,6 +128,7 @@ class SAFull(nn.Module):
         init.kaiming_normal_(self.key_conv.weight, mode='fan_out', nonlinearity='relu')
         init.kaiming_normal_(self.value_conv.weight, mode='fan_out', nonlinearity='relu')
         init.kaiming_normal_(self.query_conv.weight, mode='fan_out', nonlinearity='relu')
+
 
 class SAPooling(nn.Module):
     def __init__(self, channels, heads=1, bias=False):
@@ -239,7 +248,7 @@ class SAStem(nn.Module):
 class SABottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_channels, out_channels, stride=1, kernel_size=7, groups=1, base_width=64, heads=8, with_conv=False):
+    def __init__(self, in_channels, out_channels, stride=1, kernel_size=7, groups=1, base_width=64, heads=8, with_conv=False, r_dim=256):
         super(SABottleneck, self).__init__()
         self.stride = stride
         self.heads = heads
@@ -255,8 +264,8 @@ class SABottleneck(nn.Module):
         )
 
         padding = get_same_padding(kernel_size)
-        self.conv_2_1 = nn.Sequential(
-            SAConv(width, width, kernel_size=self.kernel_size, stride=self.stride, padding=padding, heads=self.heads),
+        self.sa_conv = SAConv(width, width, kernel_size=kernel_size, stride=stride, padding=padding, heads=heads, r_dim=r_dim)
+        self.non_linear = nn.Sequential(
             nn.BatchNorm2d(width),
             nn.ReLU(),
         )
@@ -280,13 +289,17 @@ class SABottleneck(nn.Module):
                 nn.BatchNorm2d(self.expansion * out_channels)
             )
 
-    def forward(self, x):
+    def conv_2_1(self, out, r):
+        out = self.sa_conv(out, r)
+        return self.non_linear(out)
+
+    def forward(self, x, r=None):
         out = self.conv1(x)
 
         if self.with_conv:
-            out = self.conv_2_1(out) + self.conv_2_2(out)
+            out = self.conv_2_1(out, r) + self.conv_2_2(out)
         else:
-            out = self.conv_2_1(out)
+            out = self.conv_2_1(out, r)
 
         out = self.conv3(out)
 

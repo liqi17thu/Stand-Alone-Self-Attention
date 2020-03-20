6488b29d101e7479b57938e7ed9f242adfb47a54
Qi Li <liqi17thu@gmail.com>
2020-03-20 14:45:29 +0800

    count time

--- time.py
+++ time.py
@@ -2,69 +2,71 @@ import torch
 import torch.nn as nn
 from lib.models.units.saUnit import SAConv
 from torch.autograd import Variable
+from lib.models.units.utils import get_same_padding
 
 import time
 
+class SANet(nn.Module):
+    def __init__(self, inplanes, planes, kernel, padding, heads):
+        super(SANet, self).__init__()
+        self.saconv = SAConv(inplanes, planes, kernel_size=kernel, padding=padding, heads=heads)
 
+    def forward(self, x):
+        return self.saconv(x)
+
+
+class ConvNet(nn.Module):
+    def __init__(self, inplanes, planes, kernel, padding, heads):
+        super(ConvNet, self).__init__()
+        self.conv = nn.Conv2d(inplanes, planes, kernel_size=kernel, padding=padding)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class SPConvNet(nn.Module):
+    def __init__(self, inplanes, planes, kernel, padding, heads):
+        super(SPConvNet, self).__init__()
+        self.spconv = nn.Conv2d(inplanes, planes, kernel_size=kernel, padding=padding, groups=planes)
+
+    def forward(self, x):
+        return self.spconv(x)
+
+
+class PoolingNet(nn.Module):
+    def __init__(self, inplanes, planes, kernel, padding, heads):
+        super(PoolingNet, self).__init__()
+        self.pooling = nn.AvgPool2d(kernel, padding=padding)
+
+    def forward(self, x):
+        return self.pooling(x)
+
+
+def time_counting(x, Net, kernel=3, heads=8, gpu=False):
+    xx = Variable(x.data.clone(), requires_grad=True)
+    b, c, h, w = xx.shape
+    pad = get_same_padding(kernel)
+    net = Net(c, c, kernel, pad, heads)
+
+    if gpu:
+        xx = xx.cuda()
+        net = net.cuda()
+
+    s = time.time()
+    for _ in range(100):
+        xx = net(xx)
+    e = time.time()
+    print(f'{net.__class__.__name__} forward: {(e - s):.2E}s')
+
+    s = time.time()
+    xx = xx.mean(3).mean(2).mean(1).mean(0)
+    xx.backward()
+    e = time.time()
+    print(f'{net.__class__.__name__} backward: {(e - s):.2E}s')
 
 # input
 x = torch.rand(3, 160, 32, 32)
 
-# 4 operation
-saconv = SAConv(160, 160, kernel_size=3, heads=8, padding=1)
-conv = nn.Conv2d(160, 160, 3, padding=1)
-spconv = nn.Conv2d(160, 160, 3, padding=1, groups=160)
-pooling = nn.AvgPool2d(3, padding=1)
+time_counting(x, SPConvNet)
+time_counting(x, PoolingNet)
 
-xx = Variable(x.data.clone(), requires_grad=True)
-s = time.time()
-for _ in range(10):
-    xx = saconv(xx)
-e = time.time()
-print(f'attention forward: {e-s}s')
-
-s = time.time()
-xx = xx.mean(3).mean(2).mean(1).mean(0)
-xx.backward()
-e = time.time()
-print(f'attention backward: {e-s}s')
-
-xx = Variable(x.data.clone(), requires_grad=True)
-s = time.time()
-for _ in range(10):
-    xx = conv(xx)
-e = time.time()
-print(f'conv forward: {e-s}s')
-
-s = time.time()
-xx = xx.mean(3).mean(2).mean(1).mean(0)
-xx.backward()
-e = time.time()
-print(f'conv backward: {e-s}s')
-
-xx = Variable(x.data.clone(), requires_grad=True)
-s = time.time()
-for _ in range(100):
-    xx = spconv(xx)
-e = time.time()
-print(f'spconv forward: {e-s}s')
-
-s = time.time()
-xx = xx.mean(3).mean(2).mean(1).mean(0)
-xx.backward()
-e = time.time()
-print(f'spconv backward: {e-s}s')
-
-
-xx = Variable(x.data.clone(), requires_grad=True)
-s = time.time()
-for _ in range(100):
-    xx = pooling(xx)
-e = time.time()
-print(f'pooling forward: {e-s}s')
-
-s = time.time()
-xx = xx.mean(3).mean(2).mean(1).mean(0)
-xx.backward()
-e = time.time()
-print(f'pooling backward: {e-s}s')
